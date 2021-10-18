f13a243256843003b7ff076a120ae6085c255dcd
awaisspk <iamawaisiqball@gmail.com>
2021-10-18 23:21:04 +0500

    Feat: learned basics of typescript

---
+++ dist/basics.js
@@ -0,0 +1,8 @@
+const Multiply = (a, b) => {
+    return a * b;
+};
+function add(a, b) {
+    return a + b;
+}
+let a = null;
+console.log(a);
---
+++ src/basics.ts
@@ -0,0 +1,17 @@
+const Multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let a: number = null;
+console.log(a);
+
+type carProps = {
+  name: string;
+  model: string;
+  year: number;
+  charVoltage?: number;
+};
